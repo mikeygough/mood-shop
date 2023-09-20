3c157fb1a6adff996c8a09a647987395340f5717
mikey <mikey@hey.com>
2023-09-20 17:38:55 -0500

    add item and show item functions

--- scripts.js
+++ scripts.js
@@ -34,3 +34,18 @@ for (let i = 0; i < data.length; i += 1) {
 	console.log(img) // Check the console!
 	itemsContainer.appendChild(newDiv)
 }
+
+const cart = [];
+
+function addItem(name, price) {
+	const item = {name: name,
+								price: price,
+								qty: 1}
+	
+	cart.push(item);
+}
+
+function showItems() {
+	console.log(`You have ${cart.length} items in your cart`);
+	console.log(cart);
+}
